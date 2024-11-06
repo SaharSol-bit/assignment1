9d63c0688e16243031b1dae118ded6bdd4ae6cb6
SaharSol-bit <143494942+SaharSol-bit@users.noreply.github.com>
2024-11-06 10:35:31 +0000

    data

--- main2.py
+++ main2.py
@@ -1,4 +1,6 @@
 import numpy as np
 import pandas as pd
-data=pd.read_csv('C:\Users\saraa\OneDrive\Documentos\GitHub\assignment1\swedish_population_by_year_and_sex_1860-2022.csv')
-print(data.to_string())
+data=pd.read_csv('swedish_population_by_year_and_sex_1860-2022.csv', header= 0)
+print (data['age']) 
+
+data.describe()
