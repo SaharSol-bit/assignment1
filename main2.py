f1845cd572b41ea58c9aeaed9cd2698ae9095510
saraarcelus <saraarla254@gmail.com>
2024-11-06 10:49:32 +0100

    update

---
+++ main2.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+data=pd.read_csv('C:\Users\saraa\OneDrive\Documentos\GitHub\assignment1\swedish_population_by_year_and_sex_1860-2022.csv')
+print(data.to_string())
